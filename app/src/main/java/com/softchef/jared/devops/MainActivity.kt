a309bbbbab24650197084fbfa63343c3be5eef7c
JaredChung <jardchung@softchef.com>
2021-03-08 22:48:10 +0800

    b

--- app/src/main/java/com/softchef/jared/devops/MainActivity.kt
+++ app/src/main/java/com/softchef/jared/devops/MainActivity.kt
@@ -12,12 +12,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        
+
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(toolbar)
-
-
+        
 
 
 
